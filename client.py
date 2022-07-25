86b62732c73d1bdec6b18af75e15ccef0960aa7c
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2022-07-25 10:35:28 +0200

    Laddade upp filer

---
+++ client.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+##########################################################################################
+# client.py: Example of MQTT publish, where data entered by the user is published to a
+#            specified topic at host "broker.hivemq.com".
+##########################################################################################
+import paho.mqtt.client as mqtt
+
+def main():
+   """
+   main: Connecting a client to host "broker.hivemq.com", then publishing messages
+         entered by the user to topic "python/mqtt/topics/topic1".
+   """
+   client = mqtt.Client()
+   client.connect(host = "broker.hivemq.com", port = 1883)
+   if (not client.is_connected()):
+      client.reconnect()
+   while True:
+      print("Enter a message to publish or a blank line to finish:")
+      message = readline()
+      if (message):
+         client_publish(client, "python/mqtt/topics/topic1", message)
+      else:
+         client.disconnect()
+         break
+   print("Bye!\n")
+   return
+
+def readline():
+   """
+   readline: Returing a line of text read from the terminal.
+   """
+   s = input()
+   print()
+   return s
+
+def client_publish(client, topic, message, qos = 1):
+   """
+   client_publish: Publishing a message to specified topic.
+   """
+   client.loop_start()
+   msg = client.publish(topic = topic, payload = message, qos = qos)
+   msg.wait_for_publish()
+   print("Message " + str(message) + " published!\n")
+   return
+
+# Calling the main function to start the program if this is the startup file:
+if __name__ == "__main__":
+   main()
---
+++ server.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3 
+
+"""
+server.py: Example of MQTT subscription, where received data from multiple topics at host
+           "broker.hivemq.com" is printed in the terminal.
+"""
+
+# Import directives:
+import paho.mqtt.client as mqtt 
+
+def client_on_connect(client, data, flags, result_code):
+   """
+   client_on_connect: Printing status after connection attempt, indicated via the result code,
+                      where result_code != 0 indicates an error.
+   """
+   if (result_code):
+      print("Could not connect to host " + str(client._host) + "!\n")
+   else:
+      print("Successfully connected to host " + str(client._host) + "!\n")
+   return
+
+def client_on_disconnect(client, data, flags, result_code):
+   """
+   client_on_disconnect: Printing status after disconnection, indicated via the result code,
+                         where result_code != 0 indicates unexpected disconnection.
+   """
+   if (result_code):
+      print("Unexpected disconnection from host " + str(client._host) + "!\n")
+   else:
+      print("Disconnected from host " + str(client._host) + "!\n")
+   return
+
+def client_on_message(client, data, message):
+   """
+   client_on_message: Printing latest received message and the the topic it originates from.
+   """
+   print("Received message from topic " + str(message.topic) + ": ", end = "")
+   print(message.payload.decode("utf-8"), end = "\n")
+   return
+
+def main():
+   """
+   main: Connecting a client to host "broker.hivemq.com", using a wildcard to subscribe 
+         to every topic in "python/mqtt/topics". Function pointers are used to print
+         status during connection and disconnection and printing received messages.
+   """
+   client = mqtt.Client()
+   client.on_connect = client_on_connect
+   client.on_disconnect = client_on_disconnect
+   client.on_message = client_on_message
+   client.connect(host = "broker.hivemq.com", port = 1883)
+   if not client.is_connected():
+      client.reconnect()
+   client.subscribe(topic = "python/mqtt/topics/#", qos = 1)
+   client.loop_forever()
+   return
+
+# Calling the main function to start the program if this is the startup file:
+if __name__ == "__main__":
+   main()
