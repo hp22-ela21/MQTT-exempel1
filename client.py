90500453406b4ce5a66244d20b7a1a0b963dd802
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2022-07-26 23:29:39 +0200

    Förenklade kod och lade till kommentarer

--- client.py
+++ client.py
@@ -9,18 +9,21 @@ import paho.mqtt.client as mqtt
 def main():
    """
    main: Connecting a client to host "broker.hivemq.com", then publishing messages
-         entered by the user to topic "python/mqtt/topics/topic1".
+         entered by the user to topic "python/mqtt/topics/topic1". If the user enters
+         a blink line, the client is disconnected and the program terminates.
    """
    client = mqtt.Client()
    client.connect(host = "broker.hivemq.com", port = 1883)
    if (not client.is_connected()):
       client.reconnect()
+   client.loop_start()
    while True:
       print("Enter a message to publish or a blank line to finish:")
       message = readline()
       if (message):
          client_publish(client, "python/mqtt/topics/topic1", message)
       else:
+         client.loop_stop()
          client.disconnect()
          break
    print("Bye!\n")
@@ -36,9 +39,8 @@ def readline():
 
 def client_publish(client, topic, message, qos = 1):
    """
-   client_publish: Publishing a message to specified topic.
-   """
-   client.loop_start()
+   client_publish: Publishing a message to specified topic. A thread is
+   """   
    msg = client.publish(topic = topic, payload = message, qos = qos)
    msg.wait_for_publish()
    print("Message " + str(message) + " published!\n")
--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3 
 
 ##########################################################################################
-# server.py: Example of MQTT subscription, where received data from multiple topics
-#            at host "broker.hivemq.com" is printed in the terminal.
+# server.py: Example of MQTT subscription, where received data from multiple topics at
+#            host "broker.hivemq.com" is printed in the terminal.
 ##########################################################################################
 import paho.mqtt.client as mqtt 
 
 def client_on_connect(client, data, flags, result_code):
    """
    client_on_connect: Printing status after connection attempt, indicated via the result code,
-                     where result_code != 0 indicates an error.
+                      where result_code != 0 indicates an error.
    """
    if (result_code):
       print("Could not connect to host " + str(client._host) + "!\n")
@@ -41,6 +41,10 @@ def main():
    main: Connecting a client to host "broker.hivemq.com", using a wildcard to subscribe 
          to every topic in "python/mqtt/topics". Function pointers are used to print
          status during connection and disconnection and printing received messages.
+         A thread is started to receive messages and call the correct callback function
+         by calling method loop_start. A while loop is used to run the program continuously.
+         This while loop could be skipped by calling method loop_forever instead of
+         loop_start.
    """
    client = mqtt.Client()
    client.on_connect = client_on_connect
@@ -50,7 +54,9 @@ def main():
    if not client.is_connected():
       client.reconnect()
    client.subscribe(topic = "python/mqtt/topics/#", qos = 1)
-   client.loop_forever()
+   client.loop_start()
+   while True:
+      pass
    return
 
 # Calling the main function to start the program if this is the startup file:
